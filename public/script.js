50ad298db67b4850d62e7b0939b1c994024a781f
sdarshanDeshpande-395 <sdarshan.deshpande@gmail.com>
2026-03-24 08:47:03 +0000

    updated backend

--- public/script.js
+++ public/script.js
@@ -1,3 +1,30 @@
+let total = 0;
+
+function addToCart(item, price) {
+  console.log("clicked"); // debug
+
+  const cart = document.getElementById("cart");
+
+  const li = document.createElement("li");
+  li.innerHTML = `${item} - ₹${price} 
+    <button onclick="removeItem(this, ${price})">❌</button>`;
+
+  cart.appendChild(li);
+
+  total += price;
+  updateTotal();
+}
+
+function removeItem(btn, price) {
+  btn.parentElement.remove();
+  total -= price;
+  updateTotal();
+}
+
+function updateTotal() {
+  document.getElementById("total").innerText = "Total: ₹" + total;
+}
+
 function searchFood() {
   let input = document.getElementById("searchBox").value.toLowerCase();
   let items = document.querySelectorAll(".item");
